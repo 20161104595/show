07f96afd181be0f6aea9730c0e43fd4f25fa30df
20161104595 <a20161104595@mac25.local>
2018-06-25 10:50:24 +0800

    Initial Commit

---
+++ show/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  show
+//
+//  Created by 20161104595 on 18/6/25.
+//  Copyright © 2018年 20161104595. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
