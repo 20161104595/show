559511dfd58ee1c6d2b3a4d923d121faa13982f1
20161104595 <a20161104595@mac25.local>
2018-06-25 11:46:38 +0800

    define
    定义

--- show/main.cpp
+++ show/main.cpp
@@ -6,10 +6,26 @@
 //  Copyright © 2018年 20161104595. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include<math.h>
+#include<stdlib.h>
+struct Student{
+    int name;
+    int sex;
+    int tel;
+    int program_name;
+    int clas;
+    double score[10];
+    double final_score;
+};
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    
+    
+    
+    
     return 0;
 }
